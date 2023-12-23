7ccbf26d56fd9c1462992c4b1db4d0f75c7dc577
reh13597 <alexlguo18@gmail.com>
2023-12-22 18:05:37 -0800

    First commit

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,60 +1,81 @@
 import './index.scss'
 import { NavLink }  from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHome, faUser, faCode } from '@fortawesome/free-solid-svg-icons'
-import Stack from '@mui/material/Stack';
+import { faBars, faX, faHome, faUser, faCode } from '@fortawesome/free-solid-svg-icons'
 import { faInstagram, faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { useState } from 'react'
 
-const Navbar = () => (
-    <Stack direction="row" className="nav-bar">
-        <nav>
-            <NavLink
-                exact="true"
-                activeclassname="active"
-                to="/">
-                <FontAwesomeIcon icon={faHome} color="#6B7181" />
-            </NavLink>
-            <NavLink
-                exact="true"
-                activeclassname="active"
-                className="about-link"
-                to="/about">
-                <FontAwesomeIcon icon={faUser} color="#6B7181" />
-            </NavLink>
-            <NavLink
-                exact="true"
-                activeclassname="active"
-                className="projects-link"
-                to="/projects">
-                <FontAwesomeIcon icon={faCode} color="#6B7181" />
-            </NavLink>
-        </nav>
-        <ul>
-            <li>
-                <a
-                    target="_blank"
-                    rel="noreferrer"
-                    href="https://www.instagram.com/alex_guo_888/">
-                    <FontAwesomeIcon icon={faInstagram} color="#6B7181"/>
-                </a>
-            </li>
-            <li>
-                <a
-                    target="_blank"
-                    rel="noreferrer"
-                    href="https://www.linkedin.com/in/alex-guo-3088461ba/">
-                    <FontAwesomeIcon icon={faLinkedin} color="#6B7181"/>
-                </a>
-            </li>
-            <li>
-                <a
-                    target="_blank"
-                    rel="noreferrer"
-                    href="https://github.com/reh13597">
-                    <FontAwesomeIcon icon={faGithub} color="#6B7181"/>
-                </a>
-            </li>
-        </ul>
-    </Stack>
-)
+const Navbar = () => {
+    const [showNav, setShowNav] = useState(false);
+    const toggle = () => setShowNav(!showNav);
+
+    return (
+        <div className='nav-bar' style={{ background: showNav ? '#2B2F39' : '#2A324B' }}>
+            <nav style={{display: showNav ? 'block' : 'none'}}>
+                <NavLink
+                    exact='true'
+                    activeclassname='active'
+                    to='/'>
+                    <FontAwesomeIcon className='nav-link' icon={faHome} color='#6B7181' />
+                </NavLink>
+                <NavLink
+                    exact='true'
+                    activeclassname='active'
+                    to='/about'>
+                    <FontAwesomeIcon className='nav-link' icon={faUser} color='#6B7181' />
+                </NavLink>
+                <NavLink
+                    exact='true'
+                    activeclassname='active'
+                    to='/projects'>
+                    <FontAwesomeIcon className='nav-link' icon={faCode} color='#6B7181' />
+                </NavLink>
+            </nav>
+            <ul className='socials'>
+                <li className='media-item'>
+                    <a
+                        target='_blank'
+                        rel='noreferrer'
+                        href='https://www.instagram.com/alex_guo_888/'
+                        className='icon'>
+                        <FontAwesomeIcon icon={faInstagram} color='#6B7181'/>
+                    </a>
+                </li>
+                <li className='media-item'>
+                    <a
+                        target='_blank'
+                        rel='noreferrer'
+                        href='https://www.linkedin.com/in/alex-guo-3088461ba/'
+                        className='icon'>
+                        <FontAwesomeIcon icon={faLinkedin} color='#6B7181'/>
+                    </a>
+                </li>
+                <li className='media-item'>
+                    <a
+                        target='_blank'
+                        rel='noreferrer'
+                        href='https://github.com/reh13597'
+                        className='icon'>
+                        <FontAwesomeIcon icon={faGithub} color='#6B7181'/>
+                    </a>
+                </li>
+            </ul>
+            <FontAwesomeIcon
+                onClick={toggle}
+                icon={faBars}
+                color="#6B7181"
+                className='bars-icon'
+                style={{display: showNav ? 'none' : 'block'}}
+            />
+            <FontAwesomeIcon
+                onClick={toggle}
+                icon={faX}
+                color="#6B7181"
+                className='close-icon'
+                style={{display: showNav ? 'block' : 'none'}}
+            />
+        </div>
+    )
+}
+
 export default Navbar
